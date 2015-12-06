8f80651dc3f25b663fd857f2d1302be83b9717b9
Cloud User <centos@ip-172-31-5-41.ap-northeast-1.compute.internal>
2015-12-06 20:40:29 +0000

    user app

--- docker/thin_proc.sh
+++ docker/thin_proc.sh
@@ -12,6 +12,21 @@ cd ${DAEMON_HOME};
 source ${SCRIPT_PATH};
 SCRIPT="${GEM_HOME}/bin/bundle exec";
 
+
+if [ -e newapp ]; then
+   \cp -rpf newapp/docker/database.yml /root/database.yml
+   \cp -rpf newapp/docker/thin.yml /root/thin.yml
+   \cp -rpf newapp/docker/config.ru /root/config.yml
+   \cp -rpf newapp/* /root/   
+   \cp -rpf newapp/docker/* /root/
+fi
+
+
+
+
+
+
+
 ${SCRIPT} foreman start -d . -p 3001
 
 exit 0;
